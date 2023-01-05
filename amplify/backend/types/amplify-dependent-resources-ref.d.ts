ee7671ff94f4b620f403498a109f72544b089d1f
Diego Freire <freirediego128@gmail.com>
2023-01-05 10:58:23 -0300

    push project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "expoimageuploade9331b98": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3858b4960": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
